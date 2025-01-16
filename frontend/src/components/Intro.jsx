b3202e6cace1c1a3a31375a2ae367975c8ca48f1
racm8876 <iitjee451@gmail.com>
2025-01-16 23:53:46 +0530

    Update Intro.jsx

--- frontend/src/components/Intro.jsx
+++ frontend/src/components/Intro.jsx
@@ -32,7 +32,7 @@ const Intro = () => {
 
     <div className='Intro'>
         <div className="intro-content">
-            <div><p>Join The Adventure</p></div>
+            <div className="PJoin"><p>Join The Adventure</p></div>
             <div></div>
             <div></div>
             {token ?<Link to="/community" className='intro-button'>JOIN COMMUNITY</Link>:
